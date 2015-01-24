6ff547d7ce99eead102601e23edbbc0a2ff865ff
Anonymous <JCAtkeson@gmail.com>
2015-01-24 08:54:32 -0500

    Removing MORE tabs

--- TriDivide/Program.cs
+++ TriDivide/Program.cs
@@ -3,11 +3,11 @@
 namespace TriDivide
 {
   class MainClass
-	{
+  {
     public static void Main(string[] args)
-		{
+    {
       TriDivide tdv = new TriDivide ();
       Console.WriteLine ("Hello World!");
-		}
-	}
+    }
+  }
 }
